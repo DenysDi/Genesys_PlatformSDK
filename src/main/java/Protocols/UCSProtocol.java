17f39b4dbf9431aa6d96e48863df465e3f77fe99
DenysDI <och.zlaya@gmail.com>
2020-07-23 18:40:52 +0300

    code update

--- src/main/java/Protocols/UCSProtocol.java
+++ src/main/java/Protocols/UCSProtocol.java
@@ -16,18 +16,29 @@ import java.net.URISyntaxException;
 import static Protocols.Logging.logger;
 
 public class UCSProtocol {
-    public static void main(String[] args) throws InterruptedException, ProtocolException, URISyntaxException {
+    public static void main(String[] args) {
 
         new Logging();
 
-        UniversalContactServerProtocol ucs = new UniversalContactServerProtocol(new Endpoint(new URI("tcp://DD-WIN12R2-ES:7009")));
-            ucs.setClientName               ("PSDK");
+        UniversalContactServerProtocol ucs = null;
+        try {
+            ucs = new UniversalContactServerProtocol(new Endpoint(new URI("tcp://DD-WIN12R2-ES:7009")));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        ucs.setClientName               ("PSDK");
+        try {
             ucs.open();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        SimpleSearchCriteria Thread = new SimpleSearchCriteria();
-            Thread.setAttrName              ("StartDate");
-            Thread.setAttrValue             ("2018-01-01T00:00:00.000Z");
-            Thread.setOperator              (Operators.Greater);
+        SimpleSearchCriteria Thread0 = new SimpleSearchCriteria();
+            Thread0.setAttrName              ("StartDate");
+            Thread0.setAttrValue             ("2018-01-01T00:00:00.000Z");
+            Thread0.setOperator              (Operators.Greater);
 
         SimpleSearchCriteria Thread1 = new SimpleSearchCriteria();
             Thread1.setAttrName                 ("StartDate");
@@ -35,7 +46,7 @@ public class UCSProtocol {
             Thread1.setOperator                 (Operators.Lesser);
 
         SearchCriteriaCollection sc = new SearchCriteriaCollection();
-            sc.add                          (Thread);
+            sc.add                          (Thread0);
             sc.add                          (Thread1);
 
         /*InteractionAttributes IA = new InteractionAttributes();
@@ -65,30 +76,38 @@ public class UCSProtocol {
         rgic.setIncludeAttachments      (true);
         rgic.setInteractionId           ("00005aDQ3AH1000K");*/
 
+        try {
             ucs.send                      (RILG);
-            //ucs.send                            (rdel);
-        Message resp = ucs.request    (RILG);
+            Message resp = ucs.request    (RILG);
+            logger.info            ("1st resp \n" + resp);
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        //ucs.send                            (rdel);
+
         //Message resp1 = ucs.request         (rdel);
-        logger.info            ("1st resp \n" + resp);
+
         //System.out.println                  (resp1);
 
-        java.lang.Thread.sleep(1000);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         RequestInteractionListGetNextPage nextPage1 = new RequestInteractionListGetNextPage();
             nextPage1.setReferenceId(322223);
             nextPage1.setScrollId("46");
 
+        try {
+            ucs.send(nextPage1);
+            Message resp1 = ucs.request    (nextPage1);
+            logger.info            ("2nd resp \n" + resp1);
+
+            java.lang.Thread.sleep(1000);
             ucs.send(nextPage1);
 
-        Message resp1 = ucs.request    (nextPage1);
-        logger.info            ("2nd resp \n" + resp1);
-
-
-
-        java.lang.Thread.sleep(1000);
-        ucs.send(nextPage1);
-
-        Message resp2 = ucs.request    (nextPage1);
-        logger.info            ("3rd resp \n" + resp2);
+            Message resp2 = ucs.request    (nextPage1);
+            logger.info            ("3rd resp \n" + resp2);
 
         /*RequestInteractionListGetNextPage nextPage2 = new RequestInteractionListGetNextPage();
             nextPage2.setReferenceId(322223);
@@ -99,6 +118,11 @@ public class UCSProtocol {
         Message resp2 = ucs.request    (nextPage2);
         System.out.println            ("3rd resp \n" + resp2);*/
 
-        ucs.close();
+            ucs.close();
+        } catch (ProtocolException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
